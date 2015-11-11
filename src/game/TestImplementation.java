6f3a3bb8d78ce4ec8efed29379c9fe08a748208c
Ross McMahon <rmcmahon@scottlogic.co.uk>
2015-11-11 08:26:56 +0000

    Updating dependencies, and allowing naming of team

--- src/game/TestImplementation.java
+++ src/game/TestImplementation.java
@@ -9,9 +9,7 @@ public class TestImplementation {
 
 	public static void main(String[] args) throws GameFailureException {
 
-		// TODO Refactor. Reverse this such that TradingManager takes TradStrat interface
-		// That way we don't have to set initial_capital
-		Game game = new Game(new TradingStrategy(new TradingManager(INITIAL_CAPITAL, 0)));
+		Game game = new Game("My Team", new TradingStrategy(new TradingManager(INITIAL_CAPITAL, 0)));
 		game.run();
 
 	}
