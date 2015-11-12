a223f0ea27eb70810fd4a095d63f8834a568dc5a
Andrew Polkinghorn <apolkinghorn@scottlogic.co.uk>
2015-11-12 17:12:55 +0000

    Updated TradingStrategy
    Added a constructor to indicate where
    the students can create variables.
    
    Fixed incorrect import.

--- src/trading_game/TradingStrategy.java
+++ src/trading_game/TradingStrategy.java
@@ -3,16 +3,21 @@ package trading_game;
 import game.DailyInput;
 import game.DailyOutput;
 import game.TradingManager;
-import tradingstrategy.BaseTradingStrategy;
+import game.BaseTradingStrategy;
 import exceptions.InsufficientFundsException;
 import exceptions.InsufficientSharesException;
 
 public class TradingStrategy extends BaseTradingStrategy {
-
+	
+	public TradingStrategy (){
+		// Initialise any variables needed.
+		
+	}
+	
 	@Override
 	public DailyOutput makeDailyTrade(DailyInput input) throws InsufficientFundsException, InsufficientSharesException {
-		//use the trading manager to make trades based on input
 		
+		// Use the trading manager to make trades based on input.
 		
 		DailyOutput output;
 		if (input.getDay() % 2 == 0) {
@@ -23,5 +28,4 @@ public class TradingStrategy extends BaseTradingStrategy {
 		
 		return output;
 	}
-
 }
